46ab56c7576afdd8f9e2f0eb2851683622dec3b5
austin.lynn@hpe.com <austin.lynn@hpe.com>
2016-07-07 15:14:45 -0400

    initial commit

---
+++ bulk-copier/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bulk_copier
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var sourceConnString = ConfigurationManager.ConnectionStrings["SOURCE"].ToString();
+            var destinationConnString = ConfigurationManager.ConnectionStrings["DESTINATION"].ToString();
+
+            using (var sourceConn = new SqlConnection(sourceConnString))
+            {
+                sourceConn.Open();
+                using (var sourceCmd = sourceConn.CreateCommand())
+                {
+
+                }
+            }
+
+        }
+    }
+}
